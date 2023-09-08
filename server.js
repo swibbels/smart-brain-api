a00caef55e7f5f500cdb77f347e33a530f286460
Shane Wibbelsman <shanewibbelsman@Shanes-MacBook-Air.local>
2023-09-08 13:39:52 -0500

    working on database connection

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const image = require('./controllers/image')
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors(res.header('Access-Control-Allow-Origin', '*')));
 
 app.get('/', (req, res) => {res.send('success');})
 
