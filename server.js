9582f06da82f524f673fff6681c8e82ab495284b
Shane Wibbelsman <shanewibbelsman@Shanes-Air.ht.home>
2023-09-08 19:02:49 -0500

    connecting to render database

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const db = require('knex')({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: { rejectUnauthorized: false },
+        host: process.env.DATABASE_HOST,
+        port: 5432,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PW,
+        database: process.env.DATABASE_DB
     }
 });
 
